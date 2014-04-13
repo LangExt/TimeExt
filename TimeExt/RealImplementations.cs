d4ad8f87af3ecd6b3dd24e6d80470132e1031010
otf <otf@me.com>
2014-04-13 20:42:10 +0900

    implement real.

---
+++ TimeExt/RealImplementations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TimeExt
+{
+    public sealed class RealStopwatch : IStopwatch
+    {
+        readonly Stopwatch sw = Stopwatch.StartNew();
+
+        internal RealStopwatch() { }
+
+        public TimeSpan Elapsed
+        {
+            get { return sw.Elapsed; }
+        }
+    }
+
+    public sealed class RealTimer : ITimer
+    {
+        public event EventHandler Tick;
+
+        readonly Timer timer;
+
+        internal RealTimer(TimeSpan interval) 
+        {
+            this.timer = new Timer(_ => EventHelper.Raise(Tick, this, EventArgs.Empty), null, TimeSpan.Zero, interval);
+        }
+    }
+
+    public sealed class RealTimeline : ITimeline
+    {
+        public DateTime Now
+        {
+            get { return DateTime.UtcNow; }
+        }
+
+        public void WaitForTime(TimeSpan timeSpan)
+        {
+            Thread.Sleep(timeSpan);
+        }
+
+        public IStopwatch CreateStopwatch()
+        {
+            return new RealStopwatch();
+        }
+
+        public ITimer CreateTimer(TimeSpan interval)
+        {
+            return new RealTimer(interval);
+        }
+    }
+}
