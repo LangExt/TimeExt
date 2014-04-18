b7bccb17e11c5737f8141fab57748250d88eec5f
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-04-18 15:06:27 +0900

    Waiterを切り出した

---
+++ TimeExt/VirtualImplementations/Waiter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TimeExt.VirtualImplementations
+{
+    /// <summary>
+    /// テスト用の回数によって時間が変わるwaitを提供するクラスです。
+    /// </summary>
+    public static class Waiter
+    {
+        /// <summary>
+        /// 呼び出すごとに待つ時間が異なるようなwait関数を作成します。
+        /// </summary>
+        public static Action CreateWaiter(this ITimeline tl, params TimeSpan[] timeSpans)
+        {
+            int i = 0;
+            return () =>
+            {
+                if (timeSpans.Length <= i)
+                    throw new InvalidOperationException(string.Format("{0}回より多い回数のwaitが発生しました。", timeSpans.Length));
+
+                tl.WaitForTime(timeSpans[i++]);
+            };
+        }
+
+        /// <summary>
+        /// 呼び出すごとに待つ時間が異なるようなwait関数を作成します。
+        /// それぞれの時間を数値として渡すと、関数fがそれぞれに適用されます。
+        /// 例えば、関数fにTimeSpan.FromSecondsを渡した場合、その後ろの引数は秒数を表します。
+        /// </summary>
+        public static Action CreateWaiter(this ITimeline tl, Func<double, TimeSpan> f, params double[] timeSpanValues)
+        {
+            return CreateWaiter(tl, timeSpanValues.Select(f).ToArray());
+        }
+    }
+}
