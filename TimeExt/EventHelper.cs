115048bd0367b814ac368dfbf9e62288d44f1903
otf <otf@me.com>
2014-04-19 13:01:28 +0900

    add debbuger hidden. refs #15

--- TimeExt/EventHelper.cs
+++ TimeExt/EventHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,14 @@ namespace TimeExt
 {
     internal static class EventHelper
     {
+	[DebuggerHidden]
         internal static void Raise(EventHandler handler, object sender, EventArgs args) 
         {
             if (handler != null)
                 handler(sender, args);
         }
+
+	[DebuggerHidden]
         internal static void Raise<TEventArgs>(EventHandler<TEventArgs> handler, object sender, TEventArgs args) where TEventArgs : EventArgs
         {
             if (handler != null)
